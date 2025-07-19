9eeca19b8b7d3f5cc183f71feb251458ecefaed8
Pruthv.... <pruthvirajkachate2005@gmail.com>
2025-07-19 13:32:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const eventenddate=new Date("19 July , 2025 13:30:00").getTime();
+const eventstartdate=new Date().getTime();
+
+function updateTime(){
+
+    const currdate=new Date().getTime();
+    const distprogress=currdate-eventstartdate;
+    const distpending=eventenddate-currdate;
+
+    //-ve  distance error handling
+    if (distpending < 0) {
+        clearInterval(timer);
+        document.getElementById("countdown").innerHTML =`<h2 style="color: red; font-size: 2rem;">⏰ E X P I R E D !.... </h2>`;
+        document.getElementById("progress-bar").style.width = "100%";
+        return;
+    }
+
+    //timer and sdistance logic:
+    //1 day=24*60*60*1000 milisecs....
+    const days=Math.floor(distpending/(24*60*60*1000));
+    const hrs=Math.floor(distpending%(24*60*60*1000)/(60*60*1000));
+    const mins=Math.floor(distpending%(60*60*1000)/(60*1000));
+    const secs=Math.floor(distpending%(60*1000)/(1000));
+
+    //changes in timer UI :
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hrs;
+    document.getElementById("minutes").innerHTML = mins;
+    document.getElementById("seconds").innerHTML = secs;
+
+    //width progress changes 
+    const totaldist=eventenddate-eventstartdate;
+    const percentdist=(distprogress/totaldist)*100;
+
+    //changes in width ui :
+    document.getElementById("progress-bar").style.width=percentdist+"%";
+
+    
+
+}
+
+const timer = setInterval(updateTime, 1000);
